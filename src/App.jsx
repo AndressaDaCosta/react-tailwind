4bc65ccc5725fc71ee8e2a835240d0d802bf235a
Andressa Da Costa <andressadacostan@gmail.com>
2023-01-03 20:44:56 -0300

    article

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Header from './components/Header/Header';
-
+import ArticleList from './components/ArticleList/ArticleList';
 
 function App() {
 	return (
-		<div className="w-screen h-screen">
+		<div className="w-screen h-screen sm:px-5 flex flex-col justify-start items-center">
 			<Header />
+			<ArticleList />
 		</div>
 	);
 }
